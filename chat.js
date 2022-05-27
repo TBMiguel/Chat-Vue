c953c5b524103d566a448b88608177646143dc50
TBMiguel <miguelmpjr@gmail.com>
2022-05-27 10:38:59 -0300

    Conseguindo mostrar usuários até aqui

--- chat.js
+++ chat.js
@@ -6,15 +6,32 @@ const io = require('socket.io')(http)
 app.use('syle', express.static(__dirname + '/style'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
-io.on('connection', (socket) => {   //ouvindo se alguém se mandou mensagem
-    socket.usuario = 'Convidado'
+io.on('connection', (socket) => {
+    const count = io.engine.clientsCount;
+    console.log("Quantidade de conexões: " + count)
     socket.on('mensagem', (msg) => io.emit('mensagem', { 'usuario': socket.usuario, 'mensagem': msg }))
+
     socket.on('join', (usuario) => {
         if(usuario != null){
             socket.usuario = usuario
+
+            function imprimir(user) {
+                user.forEach((user => {
+                    console.log(user);
+                    io.emit('users', socket.usuario ) //emitindo usuarios
+                }))
+            }
+        
+            const usuarios = [socket.usuario]
+            
+            imprimir(usuarios)
         } 
         socket.broadcast.emit('mensagem', { usuario: 'servidor', 'mensagem': socket.usuario + ' entrou no chat!' } )
     })
+
+    // socket.on('disconnect', () => {
+    //    socket.broadcast.emit('mensagem', { usuario: 'servidor', 'mensagem': socket.usuario + ' saiu do chat!' } )
+    //})
 })
 
 http.listen(3000, () => console.log('Ouvindo na porta 3000'))
